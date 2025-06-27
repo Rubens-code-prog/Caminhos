48a32eb8f88d1348224c2f91037899a20055234b
Rubens Gabriel <rugabriel.gabriel@gmail.com>
2025-06-27 17:29:25 -0300

    Testes com escrita de arquivos

--- primeira pasta/segunda pasta/12_lendo_escrevendo_texto.py
+++ primeira pasta/segunda pasta/12_lendo_escrevendo_texto.py
@@ -37,10 +37,45 @@ lista_compras = open(pasta_atual / 'lista de compras.txt')
 
 
 # Escrevendo arquivos
-itens_ja_comprados = ['farinha', 'fermento', 'água']
+itens_ja_comprados = ['farinha', 'fermento', 'agua']
 
 
 with open(pasta_atual / 'lista de compras.txt') as lista_compras:
     itens_lista = lista_compras.readlines()
 
-print(itens_lista)
+with open(pasta_atual / 'lista_de_compras_atualizada.txt', mode='w') as lista_atualizada:
+    for item in itens_lista:
+        if not item.replace('\n', '') in itens_ja_comprados:
+            lista_atualizada.write(item)
+
+
+
+
+# Escrevendo linha a linha
+
+
+
+with open(pasta_atual / 'lista de compras.txt') as lista_compras:
+    itens_lista = lista_compras.readlines()
+
+itens_lista_atualizada = []
+for item in itens_lista:
+    if not item.replace('\n', '') in itens_ja_comprados:
+        itens_lista_atualizada.append(item)
+
+with open('lista_de_compras_atualizada.txt', mode='w') as lista_atualizada:
+    itens_lista_atualizada[-1] = itens_lista_atualizada[-1].replace('\n', '')
+    lista_atualizada.writelines(itens_lista_atualizada)
+
+print(itens_lista_atualizada)
+
+
+
+
+# Acrescentando valores a um arquivo
+
+
+
+novos_itens = ['banana']
+with open(pasta_atual / 'lista_de_compras_atualizada.txt', mode='a') as lista_adicionada:
+    lista_adicionada.writelines(novos_itens)
